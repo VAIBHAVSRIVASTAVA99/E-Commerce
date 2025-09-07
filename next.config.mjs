3cf99e32ed7045e3dda50e5087b29f645c74608b
VAIBHAVSRIVASTAVA99 <srivastavavaibhav499@gmail.com>
2025-09-07 14:04:53 +0530

    fixed deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
